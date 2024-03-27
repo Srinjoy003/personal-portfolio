d9dfd9e26a6b1b552c103497ff771398164772b8
Srinjoy Sengupta <srinjoy003@gmail.com>
2024-03-27 09:36:49 +0530

    Finished Timeline

---
+++ app/components/timeline.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Timeline() {
+	return (
+		<section className="education" id="education">
+			<h2 className="heading">Education</h2>
+
+			<div className="timeline-items">
+				<div className="timeline-item">
+					<div className="timeline-dot"></div>
+					<div className="timeline-date">2021</div>
+					<div className="timeline-content">
+						<h3>High School</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+							magni explicabo doloremque error quibusdam recusandae accusantium
+							porro molestias odit, ut harum dicta vel corporis doloribus
+							dolore! Eius veritatis tempora provident.
+						</p>
+					</div>
+				</div>
+
+				<div className="timeline-item">
+					<div className="timeline-dot"></div>
+					<div className="timeline-date">2022</div>
+					<div className="timeline-content">
+						<h3>University</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+							magni explicabo doloremque error quibusdam recusandae accusantium
+							porro molestias odit, ut harum dicta vel corporis doloribus
+							dolore! Eius veritatis tempora provident.
+						</p>
+					</div>
+				</div>
+
+				<div className="timeline-item">
+					<div className="timeline-dot"></div>
+					<div className="timeline-date">2023</div>
+					<div className="timeline-content">
+						<h3>Internship</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+							magni explicabo doloremque error quibusdam recusandae accusantium
+							porro molestias odit, ut harum dicta vel corporis doloribus
+							dolore! Eius veritatis tempora provident.
+						</p>
+					</div>
+				</div>
+
+				<div className="timeline-item">
+					<div className="timeline-dot"></div>
+					<div className="timeline-date">2024</div>
+					<div className="timeline-content">
+						<h3>Job 1</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+							magni explicabo doloremque error quibusdam recusandae accusantium
+							porro molestias odit, ut harum dicta vel corporis doloribus
+							dolore! Eius veritatis tempora provident.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default Timeline;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Poppins } from "next/font/google";
 import Header from "./components/header";
 import Hero from "./components/hero";
+import Timeline from "./components/timeline";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<Timeline />
 		</>
 	);
 }
